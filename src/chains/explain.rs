d38d37c9547833333da2191a0ee477f2bf223c6a
Abraxas-365 <luisfmiranda8@gmail.com>
2024-04-10 16:20:46 -0500

    fix:prompt

--- src/chains/explain.rs
+++ src/chains/explain.rs
@@ -8,10 +8,9 @@ use langchain_rust::{
 const PROMPT: &str = r#"Explain the following {{os}} command: {{command}}.
 You should explain the command using bullet points, in  markdown format.
 
-Exmaple:
-command: ls -la
+Exmaple Command: ls -la
 
-explanation:
+Example Result
 • `ls`  Lists the contents of a directory. displays the names of files and directories in the current directory.
   • `-l` (part of `-la`) Displays the listing in long format, providing detailed information such as permissions, number of links, owner, group, size, and timestamp for each file and directory.
   • `-a` (part of `-la`) Includes entries that start with a dot (.), showing hidden files alongside the regular listings.
